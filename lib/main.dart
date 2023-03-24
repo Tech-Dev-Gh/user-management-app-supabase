841e1af2733a45e28ea2308b047424bdfedf3fa5
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2023-03-24 11:19:38 +0000

    update material app title

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'User Management App - Supabase',
+      title: 'User Profile App - Supabase',
     );
   }
 }
