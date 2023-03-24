37956cd23b031284b4aa106037ea3e3736439ad0
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2023-03-24 11:58:41 +0000

    show login screen on app start

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:user_management_app_supabase/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'User Profile App - Supabase',
+      home: LoginScreen(),
     );
   }
 }
